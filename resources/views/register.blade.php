def4e7005f99fc8680240c02925568957609d329
Youri Karijopawiro <yourikarijo1405@gmail.com>
2022-05-24 19:42:40 -0300

    added login and register views

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,20 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Natin Forum | Login</title>
+    <title>Natin Forum | Register</title>
 </head>
 <body>
     <main>
         <div class="login-container">
             <img src="{{asset('storage/images/natinlogo.png')}}" alt="natin logo" width="100" height="100">
-            <a href="/register">Register</a>
-            <form method="POST" action="/login">
+            <a href="/login">Login</a>
+            <form method="POST" action="/register">
                 @csrf
                 <label for="username">Username</label>
                 <input type="text" name="username" id="username" required>
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" required>
                 <label for="password">Password</label>
                 <input type="password" name="password" id="password" required>
-                <input type="submit" value="Inloggen">
+                <label for="repeat-password">Repeat Password</label>
+                <input type="password" name="repeat-password" id="repeat-password" required>
+                <input type="submit" value="Registreren">
             </form>
         </div>
     </main>
