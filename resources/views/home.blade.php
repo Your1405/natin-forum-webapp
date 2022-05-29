1e42c8d13d630712f63a103ef39de91afa5b43d4
abnormaliteit <keanutirtamenawi@hotmail.com>
2022-05-29 20:24:44 -0300

    home page broken content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,27 @@
     <link rel="stylesheet" href="{{asset('storage/styles/style home.css')}}">
 </head>
 <body>
-    <h1>Hello {{ $username }} </h1>
-    <p> Your password is: {{ $password }} </p>
+    <div class="hero">
+        <nav>
+            <img src="{{asset('storage/images/natinlogo.png')}}">
+            <h2 class="logo">NATIN<span> Forums</span></h2>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Regels</a></li>
+                <li><a href="#">Klachten</a></li>
+                <li><a href="#">Mededelingen</a></li>
+                <li><a href="#">Discussies</a></li>
+                <li><a href="#">Suggesties</a></li>
+                <li><a href="#">Info</a></li>
+            </ul>
+            <form method="GET" action="/newpost">
+            <button type="submit">New Post</button>
+            </form>
+            <form method="GET" action="/login">
+            <button class="logregbutton" type="submit">Login/Register</button>
+            </form>
+        </nav>
+    </div>
+
 </body>
 </html>
