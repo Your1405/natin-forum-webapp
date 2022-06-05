369048cc47c71a94f0e837ed90a45fdccc548d24
your1405 <yourikarijo1405@gmail.com>
2022-06-05 20:14:28 -0300

    added register error messages

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -21,6 +21,11 @@
                 </div>
                 <form id="login" class="inputbox" method="POST" action="/login">
                     @csrf
+                    @if ($loginError == 'username')
+                        <p style="color: red;" >Credentials don't match</p> 
+                    @elseif($loginError == 'password')
+                        <p style="color: red;" >Password is incorrect</p> 
+                    @endif
                     <input type="text" class="inputfield" placeholder="Username" name="username" id="username" required>
                     <input type="password" class="inputfield" placeholder="Password" name="password" id="password" required>
                     <button type="submit" class="submitbutton" value="Inloggen">Log in</button>
