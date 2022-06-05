be25d3a203e134193d1f6fbcd02c46ec2055a1d4
your1405 <yourikarijo1405@gmail.com>
2022-06-05 20:12:58 -0300

    added registering backend [NOT COMPLETE]

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -23,25 +25,49 @@ class AuthController extends Controller
                 return view('login', ['loginError'=>'username']);
             }
         } else if($request->isMethod('get')){
-            return view('login');
+            return view('login', ['loginError'=>null]);
         }
     } 
 
     function register(Request $request){
         if($request->isMethod('post')){
             $userInput = $request->all();
+            
             $username = $userInput['username'];
             $email = $userInput['email'];
             $password = $userInput['password'];
+            $repeatPassword = $userInput['repeat-password'];
             $currentTime = Carbon::now()->toDateTimeString();
 
-            DB::table('user')->insert([
-                
-            ]);
+            if($password != $repeatPassword){
+                return view('login', ['register-error'=>'nonMatchingPasswords']);
+            } else {
+                $encryptPassword = Hash::make($password);
+                DB::table('user')->insert([
+                    'username'=>$username,
+                    'email'=>$email,
+                    'password'=>$encryptPassword,
+                    'accountCreated'=>$currentTime,
+                    'accountStatus'=>1
+                ]);
 
-            return view('home')->with('username', $username)->with('password', $password); 
+                $userIdCollection = DB::table('user')->select('userId')->where('username', '=', $username)->get();
+                $userId = json_decode(json_encode($userIdCollection->toArray()[0]), true);
+                var_dump($userId);
+                session([
+                    'isLoggedIn'=>true,
+                    'userId'=>$userId['userId']
+                ]);
+
+                return redirect('/home'); 
+            }
         } else if($request->isMethod('get')){
-            return view('register');
+            return view('register', ['registerError'=>null]);
         }
+    }
+
+    function logout(Request $request){
+        Session::flush();
+        return redirect('/login');
     }
 }
