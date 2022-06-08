576347a3e7be7b34dfc193478bae4015f7446561
your1405 <yourikarijo1405@gmail.com>
2022-06-08 20:43:05 -0300

    Added new user page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
 
 class PageController extends Controller
 {
@@ -24,7 +25,36 @@ class PageController extends Controller
         }
     }
 
-    function newUser(){
-        return view('newuser');
+    function newUser(Request $request){
+        $userId = Session::get('userId');
+        if($request->session()->has('userId')){
+            if($request->isMethod('GET')){
+
+                $userName = DB::table('user')->select('username')->where('userId', '=', $userId)->get()->first();
+
+                return view('user.new', ['username' => $userName]);
+            } else if ($request->isMethod('POST')){
+                $userInput = $request->all();
+    
+                $userBday = $userInput['geboorteDatum'];
+                $userGeslacht = $userInput['geslacht'];
+                $userType = $userInput['userType'];
+                $userBio = $userInput['userBio'];
+    
+                DB::table('user_info')->insert([
+                    'userId' => $userId,
+                    'geboorteDatum' => $userBday,
+                    'geslacht' => $userGeslacht,
+                    'userType' => $userType,
+                    'userBio' => $userBio
+                ]);
+    
+                return redirect('/home');
+            }
+        } else {
+            return redirect('/login');
+        }
     }
+
+    
 }
