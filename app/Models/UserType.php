78e25cce2e9d550e2b886abff41794f3fa86c7d3
your1405 <yourikarijo1405@gmail.com>
2022-06-08 20:44:08 -0300

    added models for user and usertype

--- app/Models/UserType.php
+++ app/Models/UserType.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class UserType extends Model
 {
     use HasFactory;
 
-    protected $table = 'user';
+    protected $table = "user_type";
 
     protected $timestamps = false;
 }
