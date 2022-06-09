d70a0359755b826fbed038849571d6a9c0cf385c
abnormaliteit <keanutirtamenawi@hotmail.com>
2022-06-09 00:45:03 -0300

    reg page af

--- resources/views/user/new.blade.php
+++ resources/views/user/new.blade.php
@@ -5,32 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Nieuwe Gebruiker</title>
+    <link rel="stylesheet" href="{{asset('storage/styles/style register.css')}}">
 </head>
 <body>
     <main>
-        <div>Welkom op de NATIN Forum, {{ $username->username }}</div>
-        <form action="/user/new" method="POST">
-            @csrf
-            <label for="geboorteDatum">Geboorte Datum</label>
-            <input type="date" name="geboorteDatum">
-            <br>
-            <label for="geslacht">Geslacht</label>
-            <select name="geslacht">
-                <option value="1">Man</option>
-                <option value="2">Vrouw</option>
-                <option value="3">Liever niet zeggen</option>
-            </select>
-            <br>
-            <label for="userType">Student of Docent?</label>
-            <select name="userType">
+        <div class="pagebackg">
+            <div class="registerbox">
+            
+             <h3>Welkom op de NATIN Forum, <div class="usernamecolor">{{ $username->username }}</div></h3>
+             <form action="/user/new" method="POST">
+                @csrf
+                <label for="geboorteDatum">Geboorte Datum</label>
+                <input type="date" name="geboorteDatum">
+                <br>
+                <label for="geslacht">Geslacht</label>
+                <select name="geslacht">
+                    <option value="1">Man</option>
+                    <option value="2">Vrouw</option>
+                    <option value="3">Liever niet zeggen</option>
+                </select>
+                <br>
+               <label for="userType">Student of Docent</label>
+               <select name="userType">
                 <option value="1">Student</option>
                 <option value="2">Docent</option>
-            </select>
-            </br>
-            <label for="userBio">Over mij:</label>
-            <textarea name="userBio"></textarea>
-            <input type="submit" value="Gegevens opslaan">
-        </form> 
+               </select>
+                </br>
+                <label for="userBio">Over mij:</label>
+                <textarea name="userBio"></textarea>
+              <input class="logregbutton" type="submit" value="Gegevens opslaan">
+              </form> 
+            </div>
+        </div>
     </main>
 </body>
 </html>
