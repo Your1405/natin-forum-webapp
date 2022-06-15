630be7b451c7271386d21672299b2967d8c6c743
your1405 <yourikarijo1405@gmail.com>
2022-06-14 21:00:02 -0300

    changed login error message

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -21,9 +21,9 @@
                 </div>
                 <form id="login" class="inputbox" method="POST" action="/login">
                     @csrf
-                    @if ($loginError == 'username')
-                        <p style="color: red;" >Credentials don't match</p> 
-                    @elseif($loginError == 'password')
+                    @if ($loginError == 'userDoesNotExist')
+                        <p style="color: red;" >User doesn't exist</p> 
+                    @elseif($loginError == 'passwordIncorrect')
                         <p style="color: red;" >Password is incorrect</p> 
                     @endif
                     <input type="text" class="inputfield" placeholder="Username" name="username" id="username" required>
