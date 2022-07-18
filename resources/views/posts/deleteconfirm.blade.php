9b5fc3725223eb4bc1ce58abcd1797db9abb03c1
abnormaliteit <keanutirtamenawi@hotmail.com>
2022-07-18 02:09:38 -0300

    Update deleteconfirm.blade.php

--- resources/views/posts/deleteconfirm.blade.php
+++ resources/views/posts/deleteconfirm.blade.php
@@ -5,14 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bent u zeker?</title>
+    <link rel="stylesheet" href="{{asset('storage/styles/style 2.css')}}">
 </head>
 <body>
-    <h1>Bent u zeker dat u deze post wilt verwijderen?</h1>
-    <form action="/post/{{$postId}}/delete" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit" name="selection" value="1">Ja</button>
-        <button type="submit" name="selection" value="2">Nee</button>
-    </form>
+    <div class="container">
+        <h1>Bent u zeker dat u deze post wilt verwijderen?</h1>
+        <form action="/post/{{$postId}}/delete" method="POST">
+            @csrf
+            @method('DELETE')
+            <button class="ja-button" type="submit" name="selection" value="1">Ja</button>
+            <button class="nee-button" type="submit" name="selection" value="2">Nee</button>
+        </form>
+    </div>
 </body>
 </html>
